793bec0474b51bd4fc2e345633078dd8714e8fd8
Aaron Martinez <aaronmarti17@gmail.com>
2019-07-29 20:33:45 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
 var playlist = {Zeppelin: 'Stairway To Heaven'}
-function updatePlaylist(obj, string, title) {
-  Object.assign({}, obj, {string: title})
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, playlist, {artistName: songTitle})
 }
